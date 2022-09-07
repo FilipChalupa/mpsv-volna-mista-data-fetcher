1a02c9e182d7482000c9fa5222d13aea2de8938d
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-07 20:00:51 +0200

    Další podmínky

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ import fs from 'fs/promises'
 import data from './volna-mista.json' assert { type: 'json' }
 
 const casPredDnem = new Date()
-casPredDnem.setDate(casPredDnem.getDate() - 31)
+casPredDnem.setDate(casPredDnem.getDate() - 1)
 
 const output = data.polozky.filter((polozka) => {
-	if (polozka.datumVlozeni > casPredDnem.toISOString()) {
+	if (
+		//polozka.datumVlozeni > casPredDnem.toISOString() &&
+		polozka.profeseCzIsco.id === 'CzIsco/83221' &&
+		polozka.zamestnavatel?.nazev.toLowerCase().includes('pizz') !== true &&
+		polozka.upresnujiciInformace?.cs.toLowerCase().includes('pizz') !== true
+	) {
 		return true
 	}
 })
 
-// console.log('Počet:', output.length)
+console.log('Počet:', output.length)
 
 fs.writeFile('output.json', JSON.stringify(output, null, 2))
