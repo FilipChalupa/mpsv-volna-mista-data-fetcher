db7793aff46ca2899f3f5ffb226c4f151af74552
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-07 19:47:36 +0200

    Jen za poslední den

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import fs from 'fs/promises'
 import data from './volna-mista.json' assert { type: 'json' }
 
-const output = data.polozky.slice(0, 10)
+const casPredDnem = new Date()
+casPredDnem.setDate(casPredDnem.getDate() - 31)
+
+const output = data.polozky.filter((polozka) => {
+	if (polozka.datumVlozeni > casPredDnem.toISOString()) {
+		return true
+	}
+})
+
+// console.log('Počet:', output.length)
 
 fs.writeFile('output.json', JSON.stringify(output, null, 2))
